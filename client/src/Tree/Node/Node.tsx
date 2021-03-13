1595749415ffef55dd38104dff33635d513e41ff
GEOFAIRY <krs19@live.com>
2021-03-13 13:08:55 +1300

    using individual components to render items

--- client/src/Tree/Node/Node.tsx
+++ client/src/Tree/Node/Node.tsx
@@ -1,12 +1,29 @@
 import * as React from 'react'
 import './Node.scss'
 
-interface Props {}
+interface Props {
+    item: itemTreeDec
+}
+
+export interface itemTreeDec {
+    id: string;
+    data: Data;
+}
+
+export interface Data {
+    name: string;
+    scrap_cost: string;
+    tech_level: string;
+    type: string;
+    parent?: any;
+}
 
 interface State {}
 
 export default class Node extends React.Component<Props, State> {
     render() {
-        return <div className='Node'></div>
+        return <div className='Node'>
+            <span>{ this.props.item.data.name }</span>
+        </div>
     }
 }
--- client/src/Tree/Tree.tsx
+++ client/src/Tree/Tree.tsx
@@ -3,58 +3,58 @@ import './Tree.scss'
 import * as React from 'react'
 import axios from 'axios'
 
-//import Node from './Node/Node'
+import Node from './Node/Node'
 
 interface Props {}
 
 interface State {
-    itemTree: itemTreeDec[];
+    itemTree: itemTreeDec[]
 }
 
 export interface Data {
-    name: string;
-    scrap_cost: string;
-    tech_level: string;
-    type: string;
-    parent?: any;
+    name: string
+    scrap_cost: string
+    tech_level: string
+    type: string
+    parent?: any
 }
 
 export interface itemTreeDec {
-    id: string;
-    data: Data;
+    id: string
+    data: Data
 }
 
 export default class Tree extends React.Component<Props, State> {
     state: State = {
-        itemTree: []
-      };
+        itemTree: [],
+    }
 
     componentDidMount() {
         this.getTreeFromApi()
     }
 
     getTreeFromApi = () => {
-        axios.get('http://127.0.0.1:4941/api/v1/tree/').then((res) => {
-            return res.data
-        }).then((data) =>{
-            var itemTree = data
-            this.setState({itemTree})
-            console.log("state", this.state.itemTree)
-        }).catch((err) => {
-            console.error(err)
-        })
+        axios
+            .get('http://127.0.0.1:4941/api/v1/tree/')
+            .then((res) => {
+                return res.data
+            })
+            .then((data) => {
+                var itemTree = data
+                this.setState({ itemTree })
+                console.log('state', this.state.itemTree)
+            })
+            .catch((err) => {
+                console.error(err)
+            })
     }
 
     render() {
         return (
             <div className='Tree'>
-                {
-                    this.state.itemTree.map((item =>
-                        <th key="">
-                            {item.data.name}
-                        </th>
-                    ))
-                }
+                {this.state.itemTree.map((item) => (
+                    <Node key={item.id} item={item} />
+                ))}
             </div>
         )
     }
